13e3557fe0c2f46053ac6240c30780b3ad5c19db
Satoru Takahashi <fugasat@gmail.com>
2017-07-20 17:46:34 +0900

    SVMによる学習を実施

--- bow_svm.py
+++ bow_svm.py
@@ -4,8 +4,9 @@ import numpy as np
 import MeCab
 import pickle
 import random
-from gensim import corpora
-from gensim import models
+import time
+from gensim import corpora, models
+from sklearn import svm, grid_search
 
 
 base_path = "../livedoor"
@@ -84,7 +85,7 @@ def tokenize(text):
     return words
 
 
-def create_dataset(data, categories, num_test=100):
+def create_dataset(data, categories, per_train=0.9):
     """
     学習用データセットを作成
     """
@@ -106,11 +107,12 @@ def create_dataset(data, categories, num_test=100):
 
         # 訓練データが十分に確保できないときはエラーを出力
         num_category = len(category_label)
-        if num_test > num_category / 2:
-            raise RuntimeError("runtime error : num_test > num_category / 2")
+        num_train = num_category * per_train
+        num_test = num_category - num_train
+        if num_test < 0:
+            raise RuntimeError("runtime error : num_test < 0")
 
         # 訓練データとテストデータに分割
-        num_train = num_category - num_test
         category_data_train = category_data[:num_train]
         category_data_test = category_data[num_train:]
         category_label_train = category_label[:num_train]
@@ -121,6 +123,7 @@ def create_dataset(data, categories, num_test=100):
         data_test.extend(category_data_test)
         label_train.extend(category_label_train)
         label_test.extend(category_label_test)
+        print("category={0} : train={1} , test={2}".format(category, len(category_data_train), len(category_data_test)))
 
     return data_train, label_train, data_test, label_test
 
@@ -174,6 +177,7 @@ if __name__ == '__main__':
     """
     lsi_model = models.LsiModel(tfidf_corpus, id2word=dic, num_topics=300)
     lsi_corpus = np.array(lsi_model[tfidf_corpus])
+    lsi_corpus = lsi_corpus[:,:,1]
     with open('lsi_corpus.pickle', mode='wb') as f:
         pickle.dump(lsi_corpus, f)
     """
@@ -183,6 +187,47 @@ if __name__ == '__main__':
     print("lsi_corpus   : len={0} , len[0]={1} , value[0]={2}".format(len(lsi_corpus), len(lsi_corpus[0]), lsi_corpus[0]))
 
     # 教師データ作成
-    data_train, label_train, data_test, label_test = create_dataset(lsi_corpus, categories)
+    data_train, label_train, data_test, label_test = create_dataset(data=lsi_corpus, categories=categories, per_train=0.9)
+
+    # Scikit-learnのSVMアルゴリズムを使ってトレーニング
+    svc = svm.SVC()
+    # コストパラメータの設定（Grid Search用に複数設定）
+    cs = [0.001, 0.01, 0.1, 1, 10]
+    #cs = [0.1]
+    # RBFカーネルパラメータの設定（Grid Search用に複数設定）
+    gammas = [0.001, 0.01, 0.1, 1]
+    #gammas = [0.01]
+    parameters = {'kernel': ['rbf'], 'C': cs, 'gamma': gammas}
+    clf = grid_search.GridSearchCV(svc, parameters)
+    # data_trainは、BOWでベクトル化した各文書のリスト
+    # label_trainは、文書のカテゴリのリスト（ラベル）
+    start = time.time()
+    print("training...")
+    clf.fit(data_train, label_train)
+
+
+    # 学習した結果のスコアの確認
+    # (memo)
+    # train=100 : mean: 0.74889, std: 0.00338
+    # train=200 : mean: 0.79056, std: 0.02695
+    # train=300 : mean: 0.75852, std: 0.04206
+    # train=90% :
+    elapsed_time = time.time() - start
+    print("elapsed_time:{0}".format(elapsed_time / 60) + "[min]")
+    print('Grid Score: %s' % clf.grid_scores_)
+
+    # テストデータの実行
+    # Grid Searchを実行した場合、一番スコアの高いハイパーパラメータを使ったモデルでスコアを出してくれる
+    # (memo)
+    # train=100 : Score: 0.810376775788
+    # train=200 : Score: 0.875179340029
+    # train=300 : Score: 0.898631308811
+    # train=90% :
+    start = time.time()
+    print("check test data...")
+    score = clf.score(data_test, label_test)
+    elapsed_time = time.time() - start
+    print("elapsed_time:{0}".format(elapsed_time / 60) + "[min]")
+    print('Test Score: ' + str(score))
 
     pass
