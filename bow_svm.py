c831972d58c7cadff8d659bdc84c77bfba240494
Satoru Takahashi <fugasat@gmail.com>
2017-07-20 14:34:05 +0900

    bow/tfidf/lsiを実施

--- bow_svm.py
+++ bow_svm.py
@@ -1,16 +1,21 @@
 import os
 import codecs
 import MeCab
+import pickle
+from gensim import corpora
+from gensim import models
+
 
 base_path = "../livedoor"
 dir_names = ["dokujo-tsushin","it-life-hack","kaden-channel","livedoor-homme","movie-enter","peachy","smax","sports-watch","topic-news"]
 
-documents = []
 
 def read_corpus():
     """
     Livedoorコーパスを読み込み、文書ごとに単語ベクトルを生成
     """
+    documents = []
+    categories = []
     for category, dir_name in enumerate(dir_names):
         dir_name = os.path.join(base_path, dir_name)
         for filename in os.listdir(dir_name):
@@ -26,8 +31,23 @@ def read_corpus():
             # テキストを単語に分割
             words = tokenize(text)
             documents.append(words)
+            categories.append(category)
 
-            pass
+    return documents, categories
+
+
+def create_bow_vec(documents):
+    """
+    文章の単語リストをベクトルに変換する
+    """
+    dic = corpora.Dictionary(documents)
+
+    # 単語辞書から出現頻度の少ない単語及び出現頻度の多すぎる単語を排除
+    dic.filter_extremes(no_below=20, no_above=0.3)
+
+    # Bag of Wordsベクトルの作成
+    bow_corpus = [dic.doc2bow(d) for d in documents]
+    return bow_corpus, dic
 
 
 def tokenize(text):
@@ -63,4 +83,54 @@ def tokenize(text):
 
 
 if __name__ == '__main__':
-    read_corpus()
+    # Livedoorコーパスから文章ごとに単語リストを抽出
+    """
+    documents, categories = read_corpus()
+    with open('documents.pickle', mode='wb') as f:
+        pickle.dump(documents, f)
+    with open('categories.pickle', mode='wb') as f:
+        pickle.dump(categories, f)
+    """
+
+    with open('documents.pickle', mode='rb') as f:
+        documents = pickle.load(f)
+    with open('categories.pickle', mode='rb') as f:
+        categories = pickle.load(f)
+
+    # 単語リストを単語ベクトルに変換
+    """
+    bow_corpus, dic = create_bow_vec(documents)
+    with open('bow_corpus.pickle', mode='wb') as f:
+        pickle.dump(bow_corpus, f)
+    with open('dic.pickle', mode='wb') as f:
+        pickle.dump(dic, f)
+    """
+
+    with open('bow_corpus.pickle', mode='rb') as f:
+        bow_corpus = pickle.load(f)
+    with open('dic.pickle', mode='rb') as f:
+        dic = pickle.load(f)
+
+    # TF-IDFによる重み付け
+    """
+    tfidf_model = models.TfidfModel(bow_corpus)
+    tfidf_corpus = tfidf_model[bow_corpus]
+    with open('tfidf_corpus.pickle', mode='wb') as f:
+        pickle.dump(tfidf_corpus, f)
+    """
+
+    with open('tfidf_corpus.pickle', mode='rb') as f:
+        tfidf_corpus = pickle.load(f)
+
+    # LSIによる次元削減
+    """
+    lsi_model = models.LsiModel(tfidf_corpus, id2word=dic, num_topics=300)
+    lsi_corpus = lsi_model[tfidf_corpus]
+    with open('lsi_corpus.pickle', mode='wb') as f:
+        pickle.dump(lsi_corpus, f)
+    """
+
+    with open('lsi_corpus.pickle', mode='rb') as f:
+        lsi_corpus = pickle.load(f)
+
+    pass
